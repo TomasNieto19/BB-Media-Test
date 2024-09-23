5fa5fc3b549bd8584ed73daff09197db59b4e8b6
TomasNieto19 <tnieto852@gmail.com>
2024-09-23 16:29:49 -0300

    Feat: movies and series API

--- script.py
+++ script.py
@@ -1,56 +1,95 @@
 from playwright.sync_api import sync_playwright
+import requests
+import json
+import time
 
 def main():
+    start_time = time.time()
+
     url = "https://pluto.tv"
-    
+
+    session_response = {}
+
     try:
          with sync_playwright() as p:
 
-            browser = p.firefox.launch(headless=False)
+            browser = p.chromium.launch(headless=False)
 
             page = browser.new_page()
 
+            def handle_response(response):
+                if response.url.startswith("https://boot.pluto.tv/v4/start") and response.status == 200:
+                    session_response['body'] = response.body()
+
+            page.on('response', handle_response)
+
             page.goto(url)
 
-            page.wait_for_timeout(5000)
+            page.wait_for_timeout(1000)
 
-            page.click('a[href="/on-demand"]')
-
-            page.wait_for_timeout(5000)
-
-            page.click('span:text("Películas")')
-
-            page.wait_for_timeout(6000)
-
-            
-            page.click('a:text("View All")')
-
-            page.wait_for_timeout(5000)
-
-
-            lis = page.query_selector_all('li')
-
-            data = []
-
-            print(lis)
-            for li in lis:
-                a_tag = li.query_selector('a')
-                if a_tag:
-                    href = a_tag.get_attribute('href')
-                    print(href)
-
-            print(data)
-
-            page.wait_for_timeout(5000)
-
-            # Cerrar la página
             page.close()
 
-            # Cerrar el navegador
             browser.close()
         
     except Exception as e:
         print(e)
+
+    json_data = json.loads(session_response['body'])
+
+    jwt = json_data['sessionToken']
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {jwt}'
+    }
+    
+    response = requests.get('https://service-vod.clusters.pluto.tv/v4/vod/categories?includeItems=true&offset=1000&page=1', headers=headers)
+    
+    data = response.json()
+
+    series_category_id = "619043246d03190008131b89"
+
+    movies_category_id = "618da9791add6600071d68b0"
+
+    movies_and_series = []
+
+    for category in data["categories"]:
+        if any(main["categoryID"] == series_category_id or main["categoryID"] == movies_category_id for main in category["mainCategories"]):
+            category_name = category["name"]
+            
+            for item in category["items"]:
+                if item["type"] == "movie":
+                    movie_data = {
+                        "id": item["_id"],
+                        "name": item["name"],
+                        "description": item["description"],
+                        "duration": item["duration"],
+                        "genere": item["genre"],
+                        "type": item["type"],
+                        "category": category_name
+                    }
+                    movies_and_series.append(movie_data)
+                
+                elif item["type"] == "series":
+                    series_data = {
+                        "id": item["_id"],
+                        "name": item["name"],
+                        "description": item["description"],
+                        "genre": item["genre"],
+                        "type": item["type"],
+                        "seasonsNumbers": item["seasonsNumbers"],
+                        "category": category_name
+                    }
+                    movies_and_series.append(series_data)
+    
+    with open('movies_and_series.json', 'w', encoding='utf-8') as json_file:
+        json.dump(movies_and_series, json_file, ensure_ascii=False ,indent=4)
+
+
+
+    print(f"Execution time: {time.time() - start_time:.2f} seconds")
+
 
 
 if __name__ == "__main__":
