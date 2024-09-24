2cb5d57ff3c57de68eaf834e2a22ef2ce46391e0
TomasNieto19 <tnieto852@gmail.com>
2024-09-23 23:44:56 -0300

    Fix: unnecessary request & add live tv content guide

--- script.py
+++ script.py
@@ -10,6 +10,14 @@ def main():
 
     session_response = {}
 
+    live_tv_content_guide = {}
+
+    channels = {}
+
+    series_category_id = ""
+
+    movies_category_id = ""
+
     try:
          with sync_playwright() as p:
 
@@ -19,13 +27,32 @@ def main():
 
             def handle_response(response):
                 if response.url.startswith("https://boot.pluto.tv/v4/start") and response.status == 200:
-                    session_response['body'] = response.body()
+                    session_response['body'] = json.loads(response.body().decode('utf-8'))
+
+                if response.url.startswith("https://service-channels.clusters.pluto.tv/v2/guide/timelines") and response.status == 200:
+                    live_tv_content_guide['body'] = json.loads(response.body().decode('utf-8'))
+
+                if response.url.startswith("https://service-channels.clusters.pluto.tv/v2/guide/channels") and response.status == 200:
+                    channels['body'] = json.loads(response.body().decode('utf-8'))
 
             page.on('response', handle_response)
 
             page.goto(url)
 
-            page.wait_for_timeout(1000)
+            page.wait_for_selector('a[href="/on-demand"]')
+            page.click('a[href="/on-demand"]')
+
+            page.wait_for_selector('span:text("Películas")')
+            page.click('span:text("Películas")')
+
+            page.wait_for_selector('h3:text("Películas")')
+            movies_category_id = page.url.split("on-demand/")[1].split("?")[0]
+
+            page.wait_for_selector('span:text("Series")')
+            page.click('span:text("Series")')
+
+            page.wait_for_selector('h3:text("Series")')
+            series_category_id = page.url.split("on-demand/")[1].split("?")[0]
 
             page.close()
 
@@ -34,17 +61,9 @@ def main():
     except Exception as e:
         print(e)
 
-    json_data = json.loads(session_response['body'])
-
-    jwt = json_data['sessionToken']
 
     ## MOVIES AND SERIES REQUEST
-
-    movies_and_series_data = custom_request('https://service-vod.clusters.pluto.tv/v4/vod/categories?includeItems=true', jwt)
-
-    series_category_id = "619043246d03190008131b89"
-
-    movies_category_id = "618da9791add6600071d68b0"
+    movies_and_series_data = custom_request('https://service-vod.clusters.pluto.tv/v4/vod/categories?includeItems=true', session_response['body'].get('sessionToken'))
 
     category_ids = {series_category_id, movies_category_id}
 
@@ -85,23 +104,44 @@ def main():
     
     passage_data_to_json('movies_and_series.json', movies_and_series)
 
-    ## CHANNELS REQUEST LiveTV
 
-    channels_data = custom_request('https://service-channels.clusters.pluto.tv/v2/guide/channels', jwt)
+    ## CHANNELS LiveTV
+    channels_result = []
 
-    channels = []
+    try:
+        for channel in channels['body'].get('data', []):
+            channels_result.append({
+                "id": channel['id'],
+                "name": channel['name'],
+                "number": channel['number'],
+                "summary": channel['summary'],
+            })
+    except Exception as e:
+        print(e)
 
-    for channel in channels_data['data']:
-        channels.append({
-            "id": channel['id'],
-            "name": channel['name'],
-            "number": channel['number'],
-            "summary": channel['summary'],
-        })
+    passage_data_to_json('channels.json', channels_result)
 
-    passage_data_to_json('channels.json', channels)
 
-    print(f"Execution time: {time.time() - start_time:.2f} seconds") 
+    # LIVE TV CONTENT GUIDE
+    live_tv_content_guide_result = []
+    
+    try:
+        for item in live_tv_content_guide['body'].get('data', []):
+            for timeline in item.get('timelines', []):
+                timeline_data = {
+                    "id": timeline['_id'],
+                    "title": timeline['title'],
+                    "start": timeline['start'],
+                    "stop": timeline['stop'],
+                }
+                live_tv_content_guide_result.append(timeline_data)
+    except Exception as e:
+        print(e)
+
+    passage_data_to_json('live_tv_content_guide.json', live_tv_content_guide_result)
+
+    print(f"Execution time: {time.time() - start_time:.2f} seconds")
+    # En consola: Execution time: 10.23 seconds
 
 if __name__ == "__main__":
     main()
